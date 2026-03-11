02b05ce56a2b655530c6bc72355cbcb2c3e09731
Jonathan VUKOVICH <jonathan@eivo.fr>
2026-03-11 13:35:21 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "BRLMPrinterKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BRLMPrinterKit",
+            targets: ["BRLMPrinterKit"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BRLMPrinterKit",
+            path: "BRLMPrinterKit/BRLMPrinterKit.xcframework"
+        )
+    ]
+)
